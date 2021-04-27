aa22eacbdb7c9fc9c291cd3c0e9d7473762548b9
craigmc08 <craigmc08@gmail.com>
2021-04-27 17:18:58 -0400

    Add test mode and help message

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,28 @@ use std::path::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 && args[1] == "help" {
+        println!("Usage: drg-editor.exe [test] [filename]");
+        println!("");
+        println!("If the test positional argument is given, filename must also be passed.");
+        println!("In test mode, the program will read from the file and write to out/out.uasset");
+        println!("");
+        println!("Options:");
+        println!("  help - Print this message");
+        return;
+    }
+
+    if args.len() == 3 && args[1] == "test" {
+        // Testing mode
+
+        let mut asset = Asset::read_from(args[2].as_ref());
+        asset.recalculate_offsets();
+        asset.write_out("out/out.uasset".as_ref());
+
+        return;
+    }
+
     let asset = if args.len() == 2 {
         let asset_loc: &Path = args[1].as_ref();
         let asset = Asset::read_from(asset_loc);
